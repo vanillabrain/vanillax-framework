33ef292c6559ab532955dd98fc6ef16d298a6e28
Gaedong Kim <gaedong.k@gmail.com>
2019-01-24 22:14:02 +0900

    coreui.io vue.js based 소스 적용

---
+++ vanillax-batch-webui/README.md
@@ -0,0 +1,3 @@
+### vanillax-batch Web Client Project
+
+참조 :  https://github.com/coreui/coreui-free-vue-admin-template
---
+++ vanillax-batch-webui/src/views/users/Users.vue
@@ -0,0 +1,151 @@
+<template>
+  <b-row>
+    <b-col cols="12" xl="6">
+      <transition name="slide">
+      <b-card :header="caption">
+        <grid :rowData="data"
+          :columnData="columns"
+          :options="options"
+          @check="onCheck"
+          @uncheck="onUnCheck"
+        ></grid>
+      </b-card>
+      </transition>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import 'tui-grid/dist/tui-grid.css'
+import { Grid } from '@toast-ui/vue-grid'
+
+export default {
+    components: {
+        'grid': Grid
+    },
+    data() {
+        return {
+            columns: [
+                {
+                    title: 'Name',
+                    name: 'name'
+                },
+                {
+                    title: 'Artist',
+                    name: 'artist'
+                },
+                {
+                    title: 'Personal Score',
+                    name: 'score',
+                    onBeforeChange(ev) {
+                        console.log('executes before the value changes : ', ev);
+                    },
+                    onAfterChange(ev) {
+                        console.log('executes after the value has changed : ', ev);
+                    },
+                    copyOptions: {
+                        useListItemText: true
+                    },
+                    editOptions: {
+                        type: 'radio',
+                        listItems: [
+                            {
+                                text: '★☆☆☆☆',
+                                value: '1'
+                            },
+                            {
+                                text: '★★☆☆☆',
+                                value: '2'
+                            },
+                            {
+                                text: '★★★☆☆',
+                                value: '3'
+                            },
+                            {
+                                text: '★★★★☆',
+                                value: '4'
+                            },
+                            {
+                                text: '★★★★★',
+                                value: '5'
+                            }
+                        ],
+                        useViewMode: true
+                    }
+                }
+            ],
+            data: [
+                {
+                    name: 'Kiss and Make Up',
+                    artist: 'Dua Lipa',
+                    score: '5'
+                },
+                {
+                    name: 'Bohemian Rhapsody',
+                    artist: 'Queen',
+                    score: '2'
+                },
+                {
+                    name: 'Done For Me',
+                    artist: 'Charlie Puth',
+                    score: '3'
+                },
+                {
+                    name: 'thank u, next',
+                    artist: 'Ariana Grande',
+                    score: '4'
+                },
+                {
+                    name: 'Handclap',
+                    artist: 'Fitz & The Tantrums',
+                    score: '1'
+                },
+                {
+                    name: 'Shape Of You',
+                    artist: 'Ed Sheeran',
+                    score: '5'
+                },
+                {
+                    name: 'Snowman',
+                    artist: 'Sia',
+                    score: '5'
+                },
+                {
+                    name: 'Don\'t Stop Me Now ',
+                    artist: 'Queen',
+                    score: '3'
+                },
+                {
+                    name: 'Havana',
+                    artist: 'Camila Cabello',
+                    score: '2'
+                },
+                {
+                    name: 'A No No',
+                    artist: 'Mariah Carey',
+                    score: '5'
+                }
+            ],
+            options: {
+                rowHeaders: [
+                    {
+                        type: 'checkbox'
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        onCheck(ev) {
+            console.log('check event: ', ev);
+        },
+        onUnCheck(ev) {
+            console.log('uncheck event: ', ev);
+        }
+    }
+};
+</script>
+
+<style>
+@import 'https://uicdn.toast.com/tui-grid/latest/tui-grid.css';
+</style>
