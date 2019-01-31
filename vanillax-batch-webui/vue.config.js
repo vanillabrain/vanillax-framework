2248b6fa070a4ff077aead5e1ad7f1bdfb62a62d
Gaedong Kim <gaedong.k@gmail.com>
2019-01-31 15:06:34 +0900

    개발 로컬 서버 세팅 추가

--- vanillax-batch-webui/vue.config.js
+++ vanillax-batch-webui/vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   lintOnSave: false,
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 3000, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '^/rest': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  },
+
 }
